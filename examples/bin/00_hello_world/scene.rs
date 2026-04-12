ed8658b90591726d9e990364372bf31585c403c3
jakehuang <jakehuang@softstargames.com.tw>
2026-04-12 21:20:52 +0800

    chore(examples): refactor examples

---
+++ examples/bin/00_hello_world/scene.rs
@@ -0,0 +1,19 @@
+use crate::rfgui::ui::{RsxNode, component, rsx};
+use rfgui::{Align, Element, JustifyContent, Layout, Length};
+
+#[component]
+pub fn MainScene() -> RsxNode {
+
+    rsx! {
+        <Element
+            style={{
+                width: Length::percent(100.0),
+                height: Length::percent(100.0),
+                layout: Layout::flex().align(Align::Center).justify_content(JustifyContent::Center),
+                font_size: 100.0,
+                color: "#fff",
+            }}>
+            Hello, world!
+        </Element>
+    }
+}
